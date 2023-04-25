625d97294364cfb03a1e16000a9a08f0c18666e0
WagDevX <waguinbk@gmail.com>
2023-04-25 11:28:12 -0300

    modified:   main.kv 	modified:   tarefas/__init__.py 	new file:   tarefas/__pycache__/__init__.cpython-311.pyc 	modified:   tarefas/tarefas.kv Adicionado novo botão para atualizar tarefas e função que envia dados para o firebase

---
+++ tarefas/__init__.py
@@ -0,0 +1,23 @@
+import pyrebase
+from pyrebaseConfig import firebaseConfig
+from kivymd.uix.snackbar import Snackbar
+
+def envia_tarefas_firebase(title, desc, prior):
+        firebase = pyrebase.initialize_app(firebaseConfig)
+        auth = firebase.auth()
+        db = firebase.database()
+        user = auth.sign_in_with_email_and_password("admin@admin.com", "123456") 
+        try:
+            data = {"Titulo": title,
+                    "Descricao": desc,
+                    "Prioridade": prior,
+                    "Status": False,
+                    "Finalizada": False,
+                    "Responsável": "Ninguém"
+                    }
+            db.child("tasks").child(title.upper()).set(data, user['idToken'])
+        except Exception:
+            Snackbar(text="Erro ao enviar os dados!").open()
+        else:
+            Snackbar(text="Adicionado com sucesso!").open()
+            return True

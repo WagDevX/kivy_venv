61ec1769adc40ae9930a5058549a7bb217900210
Wagner de Araujo <124446545+WagDevX@users.noreply.github.com>
2023-05-26 14:43:44 -0300

    Update __init__.py

--- login/__init__.py
+++ login/__init__.py
@@ -5,14 +5,14 @@ import pyrebase
 
 
 firebaseConfig = {
-    "apiKey": "AIzaSyCvJ9mXa6vY6EwPiXOY1o7KjMye22k0OJA",
-    "authDomain": "inventariocob.firebaseapp.com",
-    "projectId": "inventariocob",
-    "storageBucket": "inventariocob.appspot.com",
-    "messagingSenderId": "802697439429",
-    "appId": "1:802697439429:web:846552f1ba89ed60aa68ac",
-    "measurementId": "G-0SRYWQ5YJ3",
-    "databaseURL": "https://inventariocob-default-rtdb.firebaseio.com"
+    "apiKey": "",
+    "authDomain": "",
+    "projectId": "",
+    "storageBucket": "",
+    "messagingSenderId": "",
+    "appId": "",
+    "measurementId": "",
+    "databaseURL": ""
   }
 
 def verifica_dados_firebase(self, user, password, logado_antes=False):

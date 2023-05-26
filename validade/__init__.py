fbc94cb02619f45de49a5d7afc81f86472cf2c7a
Wagner de Araujo <124446545+WagDevX@users.noreply.github.com>
2023-05-26 14:42:28 -0300

    Update __init__.py

--- validade/__init__.py
+++ validade/__init__.py
@@ -12,14 +12,14 @@ if platform =='android':
 
 
 firebaseConfig = {
-    "apiKey": "AIzaSyCvJ9mXa6vY6EwPiXOY1o7KjMye22k0OJA",
-    "authDomain": "inventariocob.firebaseapp.com",
-    "projectId": "inventariocob",
-    "storageBucket": "inventariocob.appspot.com",
-    "messagingSenderId": "802697439429",
-    "appId": "1:802697439429:web:846552f1ba89ed60aa68ac",
-    "measurementId": "G-0SRYWQ5YJ3",
-    "databaseURL": "https://inventariocob-default-rtdb.firebaseio.com"
+    "apiKey": "",
+    "authDomain": "",
+    "projectId": "",
+    "storageBucket": "",
+    "messagingSenderId": "",
+    "appId": "",
+    "measurementId": "",
+    "databaseURL": ""
   }
 
 def envia_validade_firebase(cod, desc, curva, qtd, vencimento, resp, setor):
